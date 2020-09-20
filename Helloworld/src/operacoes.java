cf4ddfdbfc57e23be8e430a1b1f9c2987f651615
GabriellRodrigues <gabriell_firminoo@hotmail.com>
2020-09-20 10:28:31 -0300

    Added pow function

--- Helloworld/src/operacoes.java
+++ Helloworld/src/operacoes.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
 public class operacoes {
-    public static void main(final String[] args) {
-        Scanner ler = new Scanner(System.in);
+    public static void main(String[] args) {
+        var ler = new Scanner(System.in);
 
         int num1;
         int num2;
         int opint;
         int escolha;
         float opdiv;
+        float base, exp;
 
         do{
             System.out.println("\nEscolha uma das opções abaixo (operações com 2 números)");
@@ -16,37 +17,60 @@ public class operacoes {
             System.out.println("2 - Subtrair;");
             System.out.println("3 - Dividir;");
             System.out.println("4 - Multiplicar;");
-            System.out.println("5 - Sair;");
+            System.out.println("5 - Potenciação;");
+            System.out.println("6 - Sair;");
             escolha = ler.nextInt();
 
-            if (escolha == 5){
+            if (escolha == 6){
                 System.out.println("Encerrando o programa...\n");
                 System.exit(0);
             }
 
-            if (escolha < 1 || escolha > 4){
+            if (escolha < 1 || escolha > 5){
                 System.out.println("Opção inválida");
             }
-
-            System.out.print("Primeiro número: ");
-            num1 = ler.nextInt();
-            System.out.print("Segundo número: ");
-            num2 = ler.nextInt();
     
             if (escolha == 1){
+                System.out.print("Primeiro número: ");
+                num1 = ler.nextInt();
+                System.out.print("Segundo número: ");
+                num2 = ler.nextInt();
+
                 opint = soma(num1, num2);
                 System.out.printf("A soma entre %d e %d é igual a %d\n", num1, num2, opint);
             }else if (escolha == 2){
+                System.out.print("Primeiro número: ");
+                num1 = ler.nextInt();
+                System.out.print("Segundo número: ");
+                num2 = ler.nextInt();
+
                 opint = subtracao(num1, num2);
                 System.out.printf("A subtação entre %d e %d é igual a %d\n", num1, num2, opint);
             }else if (escolha == 3){
+                System.out.print("Primeiro número: ");
+                num1 = ler.nextInt();
+                System.out.print("Segundo número: ");
+                num2 = ler.nextInt();
+
                 opdiv = divisao(num1, num2);
                 System.out.printf("A divisão entre %d e %d é igual a %.2f\n", num1, num2, opdiv);        
             }else if (escolha == 4){
+                System.out.print("Primeiro número: ");
+                num1 = ler.nextInt();
+                System.out.print("Segundo número: ");
+                num2 = ler.nextInt();
+
                 opint = multiplicacao(num1, num2);
                 System.out.printf("A multiplicação entre %d e %d é igual a %d\n", num1, num2, opint);
+            }else if (escolha == 5){
+                System.out.println("Base: ");
+                base = ler.nextFloat();
+                System.out.println("Expoente: ");
+                exp = ler.nextFloat();
+
+                System.out.printf("O valor de %.2f elevado a %.2f é igual a %.2f\n", base, exp, potenciacao(base, exp));
             }
-        }while(escolha != 5);
+        }while(escolha != 6);
 
     }
 
@@ -64,5 +88,15 @@ public class operacoes {
 
     public static int multiplicacao(int numero1, int numero2){
         return numero1 * numero2;
+    }
+
+    public static float potenciacao(float base, float expoente){
+        int i;
+        float retorno = 1;
+
+        for (i=0; i<expoente; i++){
+            retorno = retorno * base;
+        }
+        return retorno;
     }
 }
